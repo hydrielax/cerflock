bf77e6b3b04bf2761b65f42e6ca526f8d5fdc643
hydrielax <alexidelage@gmail.com>
2020-12-30 14:35:52 +0100

    pied-de-page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//ouvrir et fermer le menu de la barre de navigation sur mobile
 function open_close_menu() {
 	var x = document.getElementsByTagName('nav')[0];
 	if (x.className === "") {
@@ -8,15 +9,7 @@ function open_close_menu() {
 }
 
 
-
-$(document).ready(function() {
-	var body = document.getElementsByTagName('body')[0];
-	var now = new Date();
-	var heure = ('0' + now.getHours()).slice(-2);
-	if (heure > 18 || heure < 7) body.className = "dark";
-	if (window.matchMedia('(prefers-color-scheme: dark)').matches) body.className = "dark";
-});
-
+//basculer le mode jour/nuit manuellement
 function changeMode() {
 	var body = document.getElementsByTagName('body')[0];
 	if (body.className === "") {
@@ -24,4 +17,14 @@ function changeMode() {
 	} else {
 		body.className = "";
 	}
+}
+
+//choisir le mode jour/nuit automatiquement
+function autoChangeMode() {
+	var body = document.getElementsByTagName('body')[0];
+	var now = new Date();
+	var heure = ('0' + now.getHours()).slice(-2);
+	if (heure > 18 || heure < 7) body.className = "dark";
+	if (window.matchMedia('(prefers-color-scheme: dark)').matches) body.className = "dark";
+	if (window.matchMedia('(prefers-color-scheme: light)').matches) body.className = "";
 }
