1c611c176d11436addc3c296e8a74c8591c9bea2
hydrielax <alexidelage@gmail.com>
2021-08-28 23:28:54 +0200

    Oups ! It was .com

--- data/fichiers de travail/signe-quatre/index.php
+++ data/fichiers de travail/signe-quatre/index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width" />
 	<link rel="alternate" hreflang="en" href="/en/<?php echo $file; ?>" />
 	<link rel="alternate" hreflang="fr" href="/fr/<?php echo $file; ?>" />
-	<link rel="canonical" href="https://www.cerflock.herokuapp.fr/fr/<?php echo $file; ?>" />
+	<link rel="canonical" href="https://www.cerflock.herokuapp.com/fr/<?php echo $file; ?>" />
 	<link rel="manifest" href="/manifest.json">
 	<link rel="stylesheet" href="/css/books.css" />
 	<!--link href="style.css" rel="stylesheet" type="text/css" /-->
--- en/enigms/Un-scandale-en-peignoir/index.php
+++ en/enigms/Un-scandale-en-peignoir/index.php
@@ -9,7 +9,7 @@
 	<meta name="robots" content="noindex">
 	<link rel="alternate" hreflang="en" href="/en/<?php echo $file; ?>" />
 	<link rel="alternate" hreflang="fr" href="/fr/<?php echo $file; ?>" />
-	<link rel="canonical" href="https://www.cerflock.herokuapp.fr/fr/<?php echo $file; ?>" />
+	<link rel="canonical" href="https://www.cerflock.herokuapp.com/fr/<?php echo $file; ?>" />
 	<link rel="manifest" href="/manifest.json">
 	<title>Riddle n°2 - Cerf'Lock Holmes</title>
 
